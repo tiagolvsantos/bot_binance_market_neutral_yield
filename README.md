9d85c7857380eb594c2c9a264afa93f1bacedf52
Tiago Santos <3266319+tiagolvsantos@users.noreply.github.com>
2024-02-02 13:30:00 +0000

    feat: add strat

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bot_binance_market_neutral_yield
 Market neutral yield strategy for binance
+
+- rename "conf_template" to "conf"
+- fill it with the the binance api key and secret
+- run strategy.py
+
+
+# Tools 
+- https://www.coinglass.com/FundingRateHeatMap
+- https://www.coinglass.com/FundingRate
+- https://www.binance.com/en/futures/funding-history/perpetual/real-time-funding-rate
+- https://www.binance.com/en/fee/deliveryFee
---
+++ strategy.py
@@ -0,0 +1,47 @@
+import ccxt
+import json
+import time
+
+
+#####################################################
+symbol="BTC/USDT"
+usd_size = 1000
+price = 44000
+trade = False
+#####################################################
+
+conf_file = ("conf.json")
+with open(conf_file, 'r') as myfile:
+    data = myfile.read()
+    configs = json.loads(data)
+
+
+binance_spot = ccxt.binance({
+    'apiKey': configs["API_KEY"],
+    'secret': configs["SECRET_KEY"]
+    })
+
+
+binance_futures = ccxt.binance({
+    'apiKey': configs["API_KEY"],
+    'secret': configs["SECRET_KEY"],
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'future', 
+    }
+    })
+    
+try:
+    if trade:
+        trade_amount = round(1000/44000,4)
+        print(f"Create limit orders for {symbol} @{price} for {trade_amount} size")
+        binance_spot.create_limit_buy_order(symbol, trade_amount, price)
+        binance_futures.create_limit_sell_order(symbol, trade_amount, price)
+
+        print("##### Orders ########")
+        binance_spot.fetch_orders(symbol)
+        binance_futures.fetch_orders(symbol)
+except Exception as e:
+    print(e)
+    time.sleep(60)
+
